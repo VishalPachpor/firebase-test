9e92f602bf156656d2c233649c0c5027c74946e9
Vishal-143x <Vishalpatil080502@gmail.com>
2023-04-12 16:21:44 +0530

    twitter login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { authentication } from './firebase';
+import { TwitterAuthProvider, signInWithPopup } from 'firebase/auth';
 
 function App() {
+  const SignInWithTwitter = ()=>{
+    const provider = new TwitterAuthProvider();
+    console.log("function is running...")
+    signInWithPopup(authentication,provider)
+    .then((re)=> {
+      console.log(re);
+    })
+    .catch((err)=>{
+      console.log(err);
+    })
+  }
+  
   return (
     <div className="App">
-      <button>Login with twitter</button>
+      <button onClick={SignInWithTwitter}>Login with twitter</button>
     </div>
   );
 }
